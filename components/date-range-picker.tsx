fc2d2fb466c44bc1fc0ecbe0fedf627d0c93daf0
Wan Mohd Hafiz <fizyboy@gmail.com>
2023-07-19 23:27:00 +0800

    tremor range picker

--- components/date-range-picker.tsx
+++ components/date-range-picker.tsx
@@ -3,63 +3,24 @@
 import * as React from "react"
 import { addDays, format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
-import { DateRange } from "react-day-picker"
-
+import { DateRangePicker, DateRangePickerItem, DateRangePickerValue } from "@tremor/react";
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
 import { useDateRange } from "@/lib/store"
 
 export function CalendarDateRangePicker({
   className,
 }: React.HTMLAttributes<HTMLDivElement>) {
 
-  const [date, setDate] = useDateRange()
+  const [value, setValue] = useDateRange()
 
   return (
     <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            size="sm"
-            className={cn(
-              "w-[240px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="end">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
+      <DateRangePicker
+        className="max-w-md mx-auto"
+        value={value}
+        onValueChange={setValue}
+        color="rose"
+      />
     </div>
   )
 }
